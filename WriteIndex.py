95e8da871a4de60dfd7ff67438b75c530491daee
aamirShaik-MANUU <aamir.shaik@manuu.edu.in>
2022-12-01 15:19:39 +0530

    Update WriteIndex.py

--- WriteIndex.py
+++ WriteIndex.py
@@ -3,4 +3,21 @@ import os
 folders = os.listdir()
 folders.remove('.git')
 
-print(folders)
+outHtml = '<!DOCTYPE html>\
+<html lang="en">\
+<head>\
+  <title>AR Anims</title>\
+</head>\
+<body>\
+  '
+
+#link dirs
+
+
+outHtml = outHtml + '<h1> this is a test <h1>\
+    </body>\
+    </html>'
+
+f = open("index.html", 'wt')
+f.write(outHtml)
+f.close()
